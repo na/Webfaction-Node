66a3de4a0f903c32f78508a909654b472555f8de
Nate Anderson <nande12@gmail.com>
2010-08-27 16:37:54 -0700

    updated to latest node version download

--- webfaction_node_js_install.py
+++ webfaction_node_js_install.py
@@ -2,17 +2,17 @@
 #!/bin/env python
 
 """
-Node v0.1.98
+Node v0.2.0
 
 node.js install/uninstall script for WebFaction.  Will install
-code from http://nodejs.org/dist/node-v0.1.98.tar.gz. This installs
-node.js along with V8 engine. 
+code from http://nodejs.org/dist/node-v0.2.0.tar.gz. This installs
+node.js along with V8 engine.
 
-To test if node is working log into your account and run the server.js script.  Then create a website with with the app mounted at '/'.  Then open 
+To test if node is working log into your account and run the server.js script.  Then create a website with with the app mounted at '/'.  Then open
 your browser and go to the website you just created and you should see "Hello World".
 
 autostart: Not Applicable
-extra info: Not Applicable 
+extra info: Not Applicable
 
 """
 
@@ -22,19 +22,19 @@ from xmlrpclib import Server
 def create(app_name, server, session_id):
 
     #define node version
-    node_version = 'node-v0.1.98'
-    
+    node_version = 'node-v0.2.0'
+
     # initial config
     home_dir = '%s/%s' % (account['home'], username)
     app_dir = '%s/webapps/%s' % (home_dir, app_name)
     bin_dir = '%s/bin' % app_dir
     src_dir = '%s/src' % app_dir
 
-    # Create new "Custom App Listening on Port" 
+    # Create new "Custom App Listening on Port"
     app = server.create_app(session_id, app_name, 'custom_app_with_port', False, '')
 
     # download and install app
-    cmd = 'cd;' 
+    cmd = 'cd;'
     cmd += 'mkdir -p %s;' % bin_dir
     cmd += 'mkdir -p %s;' % src_dir
     cmd += 'cd %s;' % src_dir
@@ -60,10 +60,10 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World');
 }).listen(%s, "127.0.0.1");
-    
+
 sys.puts('Server listening on port %s');
     """ % (bin_dir, app['port'], app['port'])
-    server.write_file(session_id, filename, _file) 
+    server.write_file(session_id, filename, _file)
 
     # change script to executable
     cmd = 'chmod 711 %s' % filename
